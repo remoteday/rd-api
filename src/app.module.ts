13c9ce269e117b0b535aaee74a28fb863d332eda
Ivan Santos <pragmaticivan@gmail.com>
2020-11-17 21:26:27 -0600

    feat: add health module and route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { RoomModule } from './room/room.module';
 import { DatabaseModule } from './common/database/database.module';
 import { TeamModule } from './team/team.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     DatabaseModule,
     RoomModule,
+    HealthModule,
     TeamModule
   ],
   controllers: [],
