1f8113b2816d12b057a8c64014196970b6d73878
Ivan Santos <pragmaticivan@gmail.com>
2020-11-21 22:55:38 -0600

    test: add test e2e setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { RoomModule } from './room/room.module';
-import { DatabaseModule } from './common/database/database.module';
+import { DatabaseModule } from './database/database.module';
 import { TeamModule } from './team/team.module';
 import { HealthModule } from './health/health.module';
+import { ConfigModule } from '@nestjs/config';
+
+const nodeEnv = process.env.NODE_ENV;
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: nodeEnv === 'test' ? '.env.test' : '.env',
+    }),
     DatabaseModule,
     RoomModule,
     HealthModule,
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -6,17 +7,26 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe());
+
+
+  app.enableShutdownHooks()
+  app.enableCors({ origin: "*" })
 
   const options = new DocumentBuilder()
     .setTitle('Remote Day API')
     .setDescription('Remote Day API')
     .setVersion('1.0')
     .build();
+
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('docs', app, document);
 
-  
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  await app.listen(configService.get<number>("SERVER_PORT"), () => {
+    console.log(`listening on http://localhost:${configService.get<number>("SERVER_PORT")}`)
+  });
 }
 bootstrap();
