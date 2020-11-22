d25d77b784ff7f30c86f29573dfa645f3dded4dd
Ivan Santos <pragmaticivan@gmail.com>
2020-11-22 11:12:29 -0600

    test: add health e2e test

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe());
 
 
