b678006a351cbaa47ed99abbdc31a6bc49b029b0
Ivan Santos <pragmaticivan@gmail.com>
2021-07-06 08:37:37 -0500

    feat: update libs and add otel

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,41 @@ import { DatabaseModule } from './database/database.module';
 import { TeamModule } from './team/team.module';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
+import { OpenTelemetryModule } from 'nestjs-otel';
+import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
+import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { BatchSpanProcessor } from '@opentelemetry/tracing';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import { diag, DiagLogLevel } from '@opentelemetry/api';
+
+// @ts-ignore
+diag.setLogger(console, DiagLogLevel.ALL);
 
 const nodeEnv = process.env.NODE_ENV;
 
+const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
+  metrics: {
+    hostMetrics: true, // Includes Host Metrics
+    defaultMetrics: true, // Includes Default Metrics
+    apiMetrics: {
+      enable: true, // Includes api metrics
+      timeBuckets: [],
+    },
+  },
+  nodeSDKConfiguration: {
+    metricExporter: new PrometheusExporter({
+      port: 8081,
+    }),
+    spanProcessor: new BatchSpanProcessor(new JaegerExporter()),
+    contextManager: new AsyncLocalStorageContextManager(),
+    instrumentations: [getNodeAutoInstrumentations()],
+  },
+});
+
 @Module({
   imports: [
+    OpenTelemetryModuleConfig,
     ConfigModule.forRoot({
       envFilePath: nodeEnv === 'test' ? '.env.test' : '.env',
     }),
@@ -20,4 +50,4 @@ const nodeEnv = process.env.NODE_ENV;
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
